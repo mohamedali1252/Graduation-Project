2b41970fd72d5631e51ee879b4c02416f35c5ffb
myrnakhaled <73798025+myrnakhaled@users.noreply.github.com>
2022-03-27 03:29:42 +0200

    Update ftppot.py

--- ftppot.py
+++ ftppot.py
@@ -5,6 +5,7 @@ from twisted.application import internet
 from twisted.protocols import basic, policies
 from twisted.internet import protocol, reactor, defer
 from twisted.python import log
+from twisted.conch.insults.insults import TerminalProtocol
 from re import match
 from twisted.internet import task
 import socket
@@ -89,9 +90,6 @@ dr--  7 web site      1894 Jan  2 03:29  ..
    
 }
 
-#global counter
-counter = 0
-
 
 
 
@@ -104,6 +102,10 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
     passdone     = 0
     loggedin     = 0
     isroot       = 0
+    su_entered   = 0
+    first_con    = 1
+    con_id       = ''
+    con_list     = [] 
 
     dir_files = {                             #dictionary of dir->files list
 
@@ -128,7 +130,8 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
 'dr--  5 web site      1024  Feb   2 04:04   .\n',
 'dr--  7 web site      1894  Jan   2 03:29   ..\n'],
 
-    }             
+    }   
+    #Features related vars          
 
     current_dir       =     'ftpdefaultdir'                            #cuurent working Dir                                                                     #Feature Extraction Table (1)
     available_dir     =   ['ftpdefaultdir' ,'dir_1','dir_2']
@@ -142,41 +145,62 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
     su_attempted      =   0                                            # 6- Su_attempted: 1 if “su root” command attempted or used; 0 otherwise.
     num_root          =   0                                            # 7- Num_root: number of operations performed as a root in the connection.
     num_access_files  =   0                                            # 8- Num_access_files: Number of operations on access control files .
-   
-   
+    num_shells        =   1
+    num_file_creations =  0                                            #second phase ISA
+    num_outbound_cmds   =  0
+    is_hot_login       =  0
+    is_guest_login     =  0
+    hot                =  0                                            #Num HOT indicators such as: entering a system directory, creating programs and executing programs.
+    num_compromised_files = 0 
+    ftplog =   ftplog = open("ftplog.txt", "w")                                       #second phase ISA
    
     def sendLine(self, msg):
-      basic.LineOnlyReceiver.sendLine(self, msg+b'\r')
+      basic.LineOnlyReceiver.sendLine(self, msg)
       
       
 
-    def reply(self, key):
+    def reply(self, key,sec_line):
         #msg = RESPONSE[key] % args
-        msg = RESPONSE[key]
-        self.sendLine(msg)
+          
+          msg = RESPONSE[key]
+          self.sendLine(msg)
+          if sec_line == 1:
+              self.transport.write(b'ftp> ')
+          
+        
+    
 
     def connectionMade(self):
         self.count = self.count + 1
-        global counter
-        counter += 1
-        log.msg('connection made')
-        log.msg('con_start_time')
+ 
         self.con_start_time = time.time()
-        log.msg(time.time())
-         #F2:protocol type
-        log.msg('protocol_type: tcp')
-         
-         #F3: Service
-        log.msg('Service: ftp')
-         
+        self.con_id += str(self.con_start_time)
+        self.con_id += ' '
+        self.con_id += self.transport.getHost().host
+        self.con_id += ' '
+        self.con_id += '23'
+        self.con_id += ' '
+        self.con_id+= self.transport.getPeer().host
+        self.con_id += ' '
+        self.con_id +=str(self.transport.getPeer().port)
+        #clear the con_list
+        self.con_list.clear()
+        self.con_list.append(self.con_id)
 
-        line = "COMMAND : TIME : IP : PARAMS"
-        log.msg(line)
-        self.reply(WELCOME_MSG)
-        #packet sniffer
+        if self.first_con == 1:
+            self.first_con = 0
+      
+        self.reply(WELCOME_MSG,0)
+       
+        #create a log file
+        if self.first_con == 0:
+            self.ftplog = open("ftplog.txt", "a")
+
+        	
+		
+        
 
        
-
 
 
     def connectionLost(self, reason):
@@ -184,20 +208,50 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
         #F1: Duration
         duration =   time.time() - self.con_start_time
         log.msg('duration=%s'% duration)
-
-        #F7 land:
-        
         log.msg(self.transport.getPeer().host)
-        log.msg(socket.gethostbyname(socket.gethostname()))
 
-        #log all table 2 features here
-        # 1- loggedin bool
-        # 2- num_failed_logins int
-        # 3- is_hot_login   <<if isanonymous = 0 || isuser == 1
-        # 4- is_guest_login << if isanonymous = 1
-        # 5- Root_shell: 1 if root shell is obtained; 0 otherwise.
-        # 6- Su_attempted: 1 if “su root” command attempted or used; 0 otherwise.
-        # 7- Num_root: number of operations performed as a root in the connection.
+        #content related features
+        self.con_list.append(',')
+        self.con_list.append(self.hot)
+        self.con_list.append(',')
+        self.con_list.append(self.num_failed_logins)
+        self.con_list.append(',')
+        self.con_list.append(self.loggedin)
+        self.con_list.append(',')
+        self.con_list.append(self.num_compromised_files)
+        self.con_list.append(',')
+        self.con_list.append(self.root_shell)
+        self.con_list.append(',')
+        self.con_list.append(self.su_attempted)
+        self.con_list.append(',')
+        self.con_list.append(self.num_root)
+        self.con_list.append(',')
+        self.con_list.append(self.num_file_creations)
+        self.con_list.append(',')
+        self.con_list.append(self.num_shells)
+        self.con_list.append(',')
+        self.con_list.append(self.num_access_files)
+        self.con_list.append(',')
+        self.con_list.append(self.num_outbound_cmds)
+        self.con_list.append(',')
+        self.con_list.append(self.is_hot_login)
+        self.con_list.append(',')
+        self.con_list.append(self.is_guest_login)
+        self.con_list.append('\n')
+        
+
+
+        for i in range(0,len(self.con_list)):
+            if i > 0:
+                self.ftplog.write(str(self.con_list[i]))
+                print(str(self.con_list[i]))
+            else:
+                self.ftplog.write(self.con_list[i])
+
+          
+        self.ftplog.close()
+
+
         
 
         
@@ -207,7 +261,7 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
 
     def timeoutConnection(self):
         line = "ConnectionTimeout: %s : %s : %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().host, self.username.decode("utf8"),self.password.decode("utf8"))
-        log.msg(line)
+        #log.msg(line)
         self.transport.loseConnection()
 
     def lineReceived(self, line):
@@ -217,14 +271,14 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
             #if err.check(FTPCmdError):
             #    self.sendLine(err.value.response())
             #else:
-            log.msg("Unexpected FTP error")
+            #log.msg("Unexpected FTP error")
             log.err(err)
 
         def processSucceeded(result):
             if isinstance(result, tuple):
-                self.reply(*result)
+                self.reply(*result,1)
             elif result is not None:
-                self.reply(result)
+                self.reply(result,1)
 
         d = defer.maybeDeferred(self.processCommand, line)
         d.addCallbacks(processSucceeded, processFailed)
@@ -278,41 +332,72 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
       #commands that need credintials
         elif self.loggedin == 1:
             if cmd == b'HELP':
+              if self.root_shell == 1:
+                  self.num_root +=1
               return self.ftp_HELP()
 
             elif cmd == b'ASCII':
+              if self.root_shell == 1:
+                  self.num_root +=1
               return self.ftp_ASCII()
 
             elif cmd == b'BINARY':
+              if self.root_shell == 1:
+                  self.num_root +=1
               return self.ftp_BINARY()
 
             elif cmd == b'BYE':
+              if self.root_shell == 1:
+                  self.num_root +=1
               return self.ftp_QUIT()
 
             elif cmd == b'QUIT':
+              if self.root_shell == 1:
+                  self.num_root +=1
               return self.ftp_QUIT()
 
             elif cmd == b'LS':
+              if self.root_shell == 1:
+                  self.num_root +=1
               return self.ftp_LS()
 
             elif cmd == b'PWD':
+              if self.root_shell == 1:
+                  self.num_root +=1
               return self.ftp_PWD()
 
             elif cmd == b'CWD':
+                if self.root_shell == 1:
+                  self.num_root +=1
                 if args == b'':
                     return PLEASE_SPECIFY_DIR
                 else:
                    return self.ftp_CWD(args)
             elif cmd == b'PUT':
+                if self.root_shell == 1:
+                  self.num_root +=1
                 if args == b'':
                     return PLEASE_SPECIFY_LocalFile_Path
                 else:
                     return self.ftp_PUT(args)
 
-            elif cmd == b'SU ROOT':
-                self.reply(PLEASE_SPECIFY_PASS)
-                return self.ftp_SU(args)
+            elif cmd == b'SU':
+                self.transport.write(b'Password: ')
+                self.su_entered = 1
+                self.su_attempted =1
+                
+                
+                
+            elif cmd == b'ROOT' and  self.su_entered == 1:
+                self.root_shell = 1
+                self.num_root +=1
+                self.is_hot_login = 1
+
+                return self.ftp_SU()
+
             elif cmd == b'SITE CHMOD':
+                if self.root_shell == 1:
+                  self.num_root +=1
                 if self.root_shell == 0:
                     self.num_access_files += 1
                     return NEED_ROOT
@@ -321,16 +406,21 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
 
 
             else:
-              return UNKNOWN_COMMAND
+              #return UNKNOWN_COMMAND
+              self.sendLine(b'500 Unknown command.')
+              log.msg(cmd)
+              log.msg(args)
+              self.transport.write(b'ftp> ')
 
 
 
     def ftp_USER(self, username):
         self.username = username
         line = "USER : %s : %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().port, self.username.decode("utf8"))
-        log.msg(line)
+        #log.msg(line)
         if self.username.decode("utf8") == 'anonymous':
              self.isanonymous = 1
+             self.is_guest_login = 1
                 
         else:
             self.isanonymous = 0
@@ -341,27 +431,36 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
         else:
             self.isuser = 0
 
-        return USER_OK_NEED_PASS
+        self.reply(USER_OK_NEED_PASS,0)
 
     def ftp_PASS(self, password):
          line = "PASS : %s : %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().host, password.decode("utf8"))
-         log.msg(line)
+         #log.msg(line)
          if bool(self.isanonymous) :
             self.loggedin = 1
-            return LOGIN_ANONYMOUS_SUCCEED
+            
+            #return LOGIN_ANONYMOUS_SUCCEED
+            #self.sendLine(b'230 User anonymous logged in\nRemote System type is UNIX\nusing binary mode to transfer files')
+            #self.transport.write(b'ftp> ')
+            self.reply(LOGIN_ANONYMOUS_SUCCEED,1)
          elif bool(self.isuser) and password.decode("utf8") == 'adminadmin':
              self.loggedin = 1
-             return LOGIN_SUCCEED
+             
+             #return LOGIN_SUCCEED
+             self.reply(LOGIN_SUCCEED,1)
+             #self.sendLine( b'230 User logged in\nRemote System type is UNIX\nusing binary mode to transfer files')
+             #self.transport.write(b'ftp> ')
          else:
              self.loggedin = 0
              del self.username
              self.num_failed_logins += 1 
-             return LOGIN_FAIL
+             #return LOGIN_FAIL
+             self.reply(LOGIN_FAIL,0)
 
 
     def ftp_FEAT(self, line):
         line = "FEAT: %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().host)
-        log.msg(line)
+        #log.msg(line)
         self.sendLine(b'211-Features:')
         for i in b'EPRT,EPSV,MDTM,PASV,REST STREAM,SIZE,TVFS,UTF8'.split(b','):
             self.sendLine(b' %s' % i)
@@ -369,51 +468,62 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
     
     def ftp_HELP(self):
        
-        log.msg("HELP: %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().host))
-        self.reply(HELP_MSG)
+        #log.msg("HELP: %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().host))
+        
+        if self.loggedin == 1:
+            self.reply(HELP_MSG,1)
+        else:
+            self.reply(HELP_MSG,0)
+
+        
+
+        
 
     def ftp_QUIT(self):
         line = "Quit: %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().host)
         log.msg(line)
-        self.reply(GOODBYE_MSG)
+        self.reply(GOODBYE_MSG,0)
         self.transport.loseConnection()
         self.disconnected = True
 
     def ftp_ASCII(self):
         line = "ASCII : %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().host)
-        log.msg(line)
-        self.reply(ASCII_COMMAND)
+        #log.msg(line)
+        self.reply(ASCII_COMMAND,1)
 
     def ftp_BINARY(self):
         line = "BINARY : %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().host)
-        log.msg(line)
-        self.reply(BINARY_COMMAND)
+        #log.msg(line)
+        self.reply(BINARY_COMMAND,1)
     def ftp_LS(self):
         line = "LS : %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().host)
-        log.msg(line)
+        #log.msg(line)
         reply = ''
         for f in  self.dir_files[self.current_dir]:
             reply += f
         basic.LineOnlyReceiver.sendLine(self, reply.encode('UTF-8')+b'\r')
+        self.transport.write(b'ftp> ')
       
     
     def ftp_PWD(self):
         line = "PWD : %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().host)
-        log.msg(line)
+        #log.msg(line)
         reply = '257/' + self.current_dir + ' is the current directory.'
         self.sendLine(reply.encode('UTF-8'))
+        self.transport.write(b'ftp> ')
 
     def ftp_CWD(self,dir):
         line = "CWD : %s : %s\n" % (strftime('%F %T'), self.transport.getPeer().host)
-        log.msg(line)
-        log.msg(dir)
+        #log.msg(line)
+        #log.msg(dir)
        
         if self.current_dir in self.available_dir:
              self.current_dir = dir.decode()
-             self.reply(CWD_COMMAND)
+             self.reply(CWD_COMMAND,1)
         else:
             reply = '550 No Such File or Directory'
             self.sendLine(reply.encode('UTF-8'))
+            self.transport.write(b'ftp> ')
 
         
 
@@ -430,20 +540,23 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
 
   
         self.dir_files[self.current_dir].append(reply)
-        self.reply(PUT_COMMAND)
+        self.reply(PUT_COMMAND,1)
 
     
-    def ftp_SU(self,args):
+    def ftp_SU(self):
         self.num_root +=1
-        self.su_attempted = 1
+    
+
         hotlist = ['root','admin','su root']
+        self.reply(SU_COMMAND,1)
+        """
         if args.decode() in hotlist:
             self.root_shell = 1
-            self.reply(SU_COMMAND)
+            self.reply(SU_COMMAND,1)
         else:
             self.root_shell = 0
-            self.reply(SU_ERROR)
-    
+            self.reply(SU_ERROR,1)
+        """
     def ftp_CHMOD(self):
         self.num_access_files += 1
 
@@ -451,11 +564,7 @@ class FTPpot(basic.LineOnlyReceiver, policies.TimeoutMixin):
 
 
     
-    #Feature Extraction table-3 
-    def runEveryTwoSecond(self):
-      print('2 sec passed number of connections = ')
-      print(self.count)
-      self.count = 0
+
         
 
     
@@ -468,18 +577,12 @@ class ftpFactory(protocol.ServerFactory):
 
 
 
-def runEveryTwoSecond():
-    global counter
-    print('number of connections in the last 2 seconds =',counter)
-    counter = 0
 
-l = task.LoopingCall(runEveryTwoSecond)
-l.start(60.0) # call every minute!
 
 
 
 log.startLogging(open('ftp.log','w'))
-reactor.listenTCP(22,ftpFactory())
+reactor.listenTCP(23,ftpFactory())
 
 
 
