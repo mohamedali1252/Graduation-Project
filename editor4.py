2bb4fb17a836a918d661a1d956441ad4cbac5498
myrnakhaled <73798025+myrnakhaled@users.noreply.github.com>
2022-04-27 05:11:07 +0200

    Add files via upload

---
+++ editor4.py
@@ -0,0 +1,134 @@
+import sys
+import time
+import os
+from time import strftime
+import datetime
+from tablethree_editor4 import table_three
+
+
+
+
+
+def edit_features():
+	connections = []
+	connections_after = []
+	connections_count = 0
+	con_array = []
+	line2 = ''
+	
+	if len(sys.argv) != 2:
+		return 1
+	else:
+		read = sys.argv[1]
+		file_csv = open(read, "r")
+		for x in file_csv:
+			connections.append(x)
+			con_array.append(x.split()) #table3
+			connections_count = connections_count + 1
+		file_csv.close()
+		for counter in range(0,connections_count):
+			line1 = connections[counter]
+			sep_conn = line1.split(",")
+			conn_no = sep_conn[0]
+			start_time = sep_conn[1]
+			orig_p = sep_conn[2]
+			resp_p = sep_conn[3]
+			orig_h = sep_conn[4]
+			resp_h = sep_conn[5]
+			duration = sep_conn[6]
+			protocol = sep_conn[7]
+			service = sep_conn[8]
+			unknown = sep_conn[9]
+			flag = sep_conn[10]
+			src_bytes = sep_conn[11]
+			dst_bytes = sep_conn[12]
+			land = sep_conn[13]
+			wrong_fragment = sep_conn[14]
+			urgent = sep_conn[15]
+			
+			total = float(start_time) + 6*60*60 + float(duration)
+			t  = time.localtime(total)
+			time_string = time.strftime("[%d/%b/%Y %H:%M:%S]", t)
+			
+			hot = sep_conn[16]
+			num_failed_logins = sep_conn[17]
+			logged_in= sep_conn[18]
+			num_compromised=sep_conn[19]
+			root_shell = sep_conn[20]
+			su_attempted = sep_conn[21]
+			num_root = sep_conn[22]
+			num_file_creations = sep_conn[23]
+			num_shells = sep_conn[24]
+			num_access_files = sep_conn[25]
+			num_outbound_cmds = sep_conn[26]
+			is_hot_login = sep_conn[27]
+			is_guest_login = sep_conn[28]
+			line2 = ''
+			
+			if service =="ssh" or resp_p=="22" :
+				file_ssh = open("ssh.log", "r")
+				for x in file_ssh:
+            #[date time] src_ip src_port dst_ip dst_port,hot,num_failed_login,logged_in,num_compromised_file,root_shell,su_attempted,num_root,num_file_creations,num_shells,num_access_files,num_outbound_cmds,is_hot_login,is_guest_login 
+					if  (time_string in x) and (orig_h in x) and  (orig_p in x) and (resp_h in x) and (resp_p in x):
+						print ("found one")
+						ssh = x.split(",")
+						hot = ssh[1]
+						num_failed_logins = ssh[2]
+						logged_in= ssh[3]
+						num_compromised=ssh[4]
+						root_shell = ssh[5]
+						su_attempted = ssh[6]
+						num_root = ssh[7]
+						num_file_creations = ssh[8]
+						num_shells = ssh[9]
+						num_access_files = ssh[10]
+						num_outbound_cmds = ssh[11]
+						is_hot_login = ssh[12]
+						is_guest_login = ssh[13]
+						line2 = duration + "," + protocol + "," +"ssh"+ "," + flag + ","+ src_bytes + ","+ dst_bytes + ","+land+ ","+wrong_fragment+ ","+urgent+ "," +hot+ "," +num_failed_logins+ "," +logged_in+ "," +num_compromised+ "," +root_shell+ "," +su_attempted+ "," +num_root+ "," +num_file_creations+ "," +num_shells+ "," +num_access_files+ "," +num_outbound_cmds+ "," +is_hot_login+ "," +is_guest_login
+						connections_after.append(line2)
+			
+			
+			
+			elif service == "ftp" or resp_p=="21":
+				print('yes INNNNNNNNNNNNNNNNNNNNNN')
+				file_ftp = open("ftplog.txt", "r")
+				for x in file_ftp:
+            #[date time] src_ip src_port dst_ip dst_port,hot,num_failed_login,logged_in,num_compromised_file,root_shell,su_attempted,num_root,num_file_creations,num_shells,num_access_files,num_outbound_cmds,is_hot_login,is_guest_login 
+					if  (time_string in x) and (orig_h in x) and  (orig_p in x) and (resp_h in x) and (resp_p in x):
+						print ("found one")
+						ftp = x.split(",")
+						hot = ftp[1]
+						num_failed_logins = ftp[2]
+						logged_in= ftp[3]
+						num_compromised=ftp[4]
+						root_shell = ftp[5]
+						su_attempted = ftp[6]
+						num_root = ftp[7]
+						num_file_creations = ftp[8]
+						num_shells = ftp[9]
+						num_access_files = ftp[10]
+						num_outbound_cmds = ftp[11]
+						is_hot_login = ftp[12]
+						is_guest_login = ftp[13]
+						line2 = duration + "," + "tcp"+ "," +"ftp"+ "," + flag + ","+ src_bytes + ","+ dst_bytes + ","+land+ ","+wrong_fragment+ ","+urgent+ "," +hot+ "," +num_failed_logins+ "," +logged_in+ "," +num_compromised+ "," +root_shell+ "," +su_attempted+ "," +num_root+ "," +num_file_creations+ "," +num_shells+ "," +num_access_files+ "," +num_outbound_cmds+ "," +is_hot_login+ "," +is_guest_login
+						connections_after.append(line2)
+			
+			
+			elif service =="smtp" or resp_p=="25":
+				line2 = duration + "," + protocol + "," +"smtp"+ "," + flag + ","+ src_bytes + ","+ dst_bytes + ","+land+ ","+wrong_fragment+ ","+urgent+ "," +hot+ "," +num_failed_logins+ "," +logged_in+ "," +num_compromised+ "," +root_shell+ "," +su_attempted+ "," +num_root+ "," +num_file_creations+ "," +num_shells+ "," +num_access_files+ "," +num_outbound_cmds+ "," +is_hot_login+ "," +is_guest_login
+				connections_after.append(line2)
+            
+
+
+       #calculate table 3 faetures for each connection and append them to connections_after
+    table_three(connections_after,sys.argv[1],2)
+
+    file_after = open("formated.txt", "w")	
+    for i in range(0,len(connections_after)):
+        file_after.write(connections_after[i])
+    file_after.close()
+			
+			
+			
+
---
+++ tablethree_editor4.py
@@ -0,0 +1,278 @@
+import sys
+import time
+import os
+from time import strftime
+import datetime
+
+
+def time_insec(epoch_time):
+		t  = time.localtime(epoch_time)
+		con_srtime = int(t.tm_sec) + float(t.tm_min) * 60
+		return con_srtime
+
+def table_three(connections_after,filename,mode):
+    
+    Count            =   []
+    Srv_count        =   []
+    Serror_rate      =   []
+    Srv_serror_rate  =   []
+    Rerror_rate      =   []
+    Srv_rerror_rate  =   []
+    Same_srv_rate    =   []
+    Diff_srv_rate    =   []
+    Srv_diff_host_rate = []
+    Dst_host_count     = []
+    Dst_host_srv_count = []
+    Dst_host_same_srv_rate = []
+    Dst_host_diff_srv_rate = []
+    Dst_host_same_src_port_rate = []
+    Dst_host_srv_diff_host_rate = []
+    Dst_host_serror_rate   = []
+    Dst_host_srv_serror_rate = []
+    Dst_host_rerror_rate = []
+    Dst_host_srv_rerror_rate = []
+
+    
+
+
+    #raed file to add table 3 Features
+    con_array = []
+    connections_count = 0
+    file_name = open(filename, "r")
+    for x in file_name:
+        con_array.append(x.split())
+        connections_count +=1
+    file_name.close()
+	
+
+
+
+
+
+    for index,con in enumerate(con_array):
+                    print('index',index+1)
+                    print("connection to check on >>",con)
+                    
+                    temp_count              = 0
+                    temp_srv_count          = 0
+                    temp_Serror_rate        = 0
+                    temp_srv_Serror_rate    = 0
+                    temp_Rerror_rate        = 0
+                    temp_Srv_rerror_rate    = 0
+                    temp_Srv_diff_host_rate = 0
+                    temp_same_srv_rate      = 0
+                    temp_diff_host_rate     = 0
+                        
+                        #starting from 32
+                    temp_Dst_host_count     = 0
+                    temp_Dst_host_srv_count = 0
+                    temp_Dst_host_same_srv_rate = 0
+                    temp_Dst_host_diff_srv_rate = 0
+                    temp_Dst_host_same_src_port_rate = 0
+                    temp_Dst_host_srv_diff_host_rate = 0
+                    temp_Dst_host_serror_rate   = 0
+                    temp_Dst_host_srv_serror_rate = 0
+                    temp_Dst_host_rerror_rate = 0
+                    temp_Dst_host_srv_rerror_rate = 0
+
+                    window_count = 0
+
+                    
+                    for con2 in con_array:
+                        if (con2 != con) & ((time_insec(float(con[1]))- time_insec(float(con2[1]) ) ) <= 2.0 ) & ((time_insec(float(con[1]))- time_insec(float(con2[1]) ) ) >= 0):
+                            
+                            #if same src (not sure) & same dst  Based on the doctor opinion and some papers 3la ALLAH ba2a
+                            if  ((con[4] == con2[4]) & (con[5] == con2[5]) )  :
+
+                                print("matched con->",time.localtime(float(con2[0])).tm_min)
+                                print(time_insec(float(con[0]))- time_insec(float(con2[0]) ) )
+                                temp_count += 1
+                                #25_Serror_rate
+                                if con2[10] in ('S0','S1','S2','S3'):
+                                    temp_Serror_rate +=1
+                                #27_Rerror_rate
+                                if con2[10] == 'REJ':
+                                    temp_Rerror_rate +=1
+                                #29_same_srv_rate
+                                if con[3] == con2[3]:
+                                    temp_same_srv_rate +=1
+
+                                        
+                                        
+                                #srv: if same src &  same dst port only??????
+                            if((con[4] == con2[4]) & (con[3] == con2[3]) ):
+                                temp_srv_count += 1
+                                #26_srv_Serror_rate
+                                if  con2[10] in ('S0','S1','S2','S3'):
+                                    temp_srv_Serror_rate +=1
+                                #28_srv_Rerror_rate
+                                if con2[10] == 'REJ':
+                                    temp_Srv_rerror_rate +=1
+                                        
+                                #31_Srv_diff_host_rate
+                                if con[5] != con2[5]:
+                                    temp_Srv_diff_host_rate +=1
+
+                    if mode == 1:
+                        #100 connction window
+                        window_count = 0
+
+                        #loop over con[0]-1 till 100 conn before
+                        start_idx = con[0] 
+                        for con2 in reversed(con_array[:start_idx]):   
+                                
+                            if con2[5] == con[5]: #consider checking against range of dst IPs later
+                                window_count +=1
+                                if window_count <= 100 :
+                                    pass
+                                else:
+                                    if con2[4] == con[4]: #same src 
+                                        temp_Dst_host_count +=1
+
+                                        if con2[3] == con[3]: #34
+                                            temp_Dst_host_same_srv_rate+=1
+                                        if con2[10] in ('S0','S1','S2','S3'): #38
+                                            temp_Dst_host_serror_rate +=1
+
+                                        if con2[10] == 'REJ': #40
+                                            temp_Dst_host_rerror_rate +=1
+
+                                    if con2[3] == con[3]: #33
+                                        temp_Dst_host_srv_count+=1
+
+                                        if  con2[2] == con[2]: #36
+                                            temp_Dst_host_same_src_port_rate+=1
+
+                                         
+                                        if con[5] != con2[5]: #37
+                                            temp_Dst_host_srv_diff_host_rate +=1
+
+                                        if con2[10] in ('S0','S1','S2','S3'): #39
+                                            temp_Dst_host_srv_serror_rate +=1
+                                        
+                                        if con2[10] == 'REJ': #41
+                                            temp_Dst_host_srv_rerror_rate +=1
+
+                    elif mode == 2:
+                        #take a window of 2 min based on avg time of probing
+                        for con2 in con_array:
+                            if (con2 != con) & ((time_insec(float(con[1]))- time_insec(float(con2[1]) ) ) <= 120.0 ) & ((time_insec(float(con[1]))- time_insec(float(con2[1]) ) ) >= 0):
+
+                                if con2[5] == con[5]: #same dst IP
+                                    temp_Dst_host_count +=1
+
+                                    if con2[3] == con[3]: #34
+                                            temp_Dst_host_same_srv_rate+=1
+                                    if con2[10] in ('S0','S1','S2','S3'): #38
+                                            temp_Dst_host_serror_rate +=1
+
+                                    if con2[10] == 'REJ': #40
+                                            temp_Dst_host_rerror_rate +=1
+
+                                if  con2[3] == con[3]: #33 same dst port only
+                                    temp_Dst_host_srv_count+=1
+
+                                    if  con2[2] == con[2]: #36
+                                        temp_Dst_host_same_src_port_rate+=1
+
+                                         
+                                    if con[5] != con2[5]: #37
+                                        temp_Dst_host_srv_diff_host_rate +=1
+
+                                    if con2[10] in ('S0','S1','S2','S3'): #39
+                                        temp_Dst_host_srv_serror_rate +=1
+                                        
+                                    if con2[10] == 'REJ': #41
+                                        temp_Dst_host_srv_rerror_rate +=1
+
+
+
+
+
+                                        
+
+
+                                            
+
+                                
+                   
+
+
+                    
+
+
+
+                            
+                    Count.append(temp_count)
+                    if temp_count != 0:
+                        Serror_rate.append(float(temp_Serror_rate)/temp_count)
+                        Rerror_rate.append(float(temp_Rerror_rate)/ temp_count)
+                        #29
+                        Same_srv_rate.append(float(temp_same_srv_rate)/temp_count)
+                        #30
+                        Diff_srv_rate.append(1- (float(temp_same_srv_rate)/temp_count))			
+                    else:
+                        Serror_rate.append(float(0))
+                        Same_srv_rate.append(float(0))
+                        Rerror_rate.append(float(0))
+                        Diff_srv_rate.append(float(0))
+
+                    Srv_count.append(temp_srv_count)
+                    if temp_srv_count != 0:
+                            Srv_serror_rate.append((float(temp_srv_count)/temp_srv_count))
+                            Srv_rerror_rate.append(float(temp_Rerror_rate)/ temp_srv_count)
+                            Srv_diff_host_rate.append(float(temp_Srv_diff_host_rate)/temp_srv_count)
+                    else:
+                            Srv_serror_rate.append(float(0))
+                            Srv_rerror_rate.append(float(0))
+                            Srv_diff_host_rate.append(float(0))
+                    #32
+                    Dst_host_count.append(temp_Dst_host_count)
+                    if temp_Dst_host_count != 0:
+                        Dst_host_same_srv_rate.append(float(temp_Dst_host_same_srv_rate)/temp_Dst_host_count)
+                        Dst_host_diff_srv_rate.append(1-(float(temp_Dst_host_same_srv_rate)/temp_Dst_host_count))
+                        Dst_host_rerror_rate.append(float(temp_Dst_host_rerror_rate)/temp_Dst_host_count)
+                        Dst_host_serror_rate.append(float(temp_Dst_host_serror_rate)/temp_Dst_host_count)
+                    else:
+                        Dst_host_same_srv_rate.append(0)
+                        Dst_host_diff_srv_rate.append(0)
+                        Dst_host_rerror_rate.append(0)
+                        Dst_host_serror_rate.append(0)
+                        
+                    #33
+                    Dst_host_srv_count.append(temp_Dst_host_srv_count)
+                    if temp_Dst_host_srv_count != 0:
+
+                        Dst_host_same_src_port_rate.append(float(temp_Dst_host_same_src_port_rate)/temp_Dst_host_srv_count)
+                        Dst_host_srv_diff_host_rate.append(float(temp_Dst_host_srv_diff_host_rate)/temp_Dst_host_srv_count)
+                        Dst_host_srv_rerror_rate.append(float(temp_Dst_host_srv_rerror_rate)/temp_Dst_host_srv_count)
+                        Dst_host_srv_serror_rate.append(float(temp_Dst_host_srv_serror_rate)/temp_Dst_host_srv_count)
+                        Dst_host_same_src_port_rate.append(float(temp_Dst_host_same_src_port_rate)/temp_Dst_host_srv_count)
+                    else:
+                        Dst_host_same_src_port_rate.append(0)
+                        Dst_host_srv_diff_host_rate.append(0)
+                        Dst_host_srv_rerror_rate.append(0)
+                        Dst_host_srv_serror_rate.append(0)
+                        Dst_host_same_src_port_rate.append(0)
+
+
+
+
+    #append to the file
+    for index,i in enumerate(connections_after):
+                temp = str(Count[index]) + "," + str(Srv_count[index]) + "," + str(Serror_rate[index]) + "," + str(Srv_serror_rate[index]) + "," + str(Rerror_rate[index]) + "," + str(Srv_rerror_rate[index]) + "," + str(Same_srv_rate[index]) + "," + str(Diff_srv_rate[index])+ "," + str(Srv_diff_host_rate[index]) + "," + str(Dst_host_count[index]) + "," +str(Dst_host_srv_count[index]) + "," + str(Dst_host_same_srv_rate[index]) + "," + str(Dst_host_diff_srv_rate[index]) + "," + str(Dst_host_same_src_port_rate[index]) + "," + str(Dst_host_srv_diff_host_rate[index]) + "," + str(Dst_host_serror_rate[index]) + "," + str(Dst_host_srv_serror_rate[index]) + "," +str(Dst_host_rerror_rate[index]) + "," + str(Dst_host_srv_rerror_rate[index]) 
+                connections_after[index] += temp
+                temp = ''
+            
+            
+            
+            
+            
+                
+    
+			
+
+                        
+                        
+                    
+                        
