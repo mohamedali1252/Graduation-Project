01eafea0d7093812947b04d94a4f20609e5b267c
mohamedali1252 <58470539+mohamedali1252@users.noreply.github.com>
2022-07-03 21:48:54 +0200

    Adding the correct topology

--- topo.py
+++ topo.py
@@ -1,11 +1,17 @@
-from mininet.topo import Topo
-from mininet.net import Mininet
-from mininet.log import setLogLevel
-from mininet.cli import CLI
-from mininet.node import OVSSwitch, Controller, RemoteController
-from mininet.util import pmonitor
-from signal import SIGINT
+#!/usr/bin/env python
 
+"""
+Example to create a Mininet topology and connect it to the internet via NAT
+"""
+
+from mininet.topo import Topo
+
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.log import lg, info
+from mininet.link import Intf
+from mininet.topolib import TreeNet
+from mininet.node import OVSSwitch, Controller, RemoteController
 
 class RoutingTopo( Topo ):
     def build(self):
@@ -13,26 +19,24 @@ class RoutingTopo( Topo ):
         h1 = self.addHost( 'h1',mac="00:00:00:00:00:01",ip="10.0.0.1/24")
         h2 = self.addHost( 'h2',mac="00:00:00:00:00:02",ip="10.0.0.2/24")
         h3 = self.addHost( 'h3',mac="00:00:00:00:00:03",ip="10.0.0.3/24")
-        s1 = self.addSwitch('s1')
+        s1 = self.addSwitch('s1',mac="00:00:00:00:00:05",ip="10.0.0.5/24")
         # Add links
         self.addLink( h1, s1 )
         self.addLink( h2, s1 )
         self.addLink( h3, s1)
+        
 
-if __name__=="__main__":
-    setLogLevel('info')
+if __name__ == '__main__':
+    lg.setLogLevel( 'info')
     topo = RoutingTopo()
     c1 = RemoteController('c1',ip='127.0.0.1')
     net = Mininet(topo=topo,controller=c1)
+    # Add NAT connectivity
+    s1 = net.switches[0]
+    net.addNAT().configDefault()
     net.start()
-    print("Dumping host connections")
-    
-    h1 = net.get('h1')
-    h2 = net.get('h2')
-    h3 = net.get('h3')
-    CLI(net)
+    info( "*** Hosts are running and should have internet connectivity\n" )
+    info( "*** Type 'exit' or control-D to shut down network\n" )
+    CLI( net )
+    # Shut down NAT
     net.stop()
-    
-#sudo python3 topo.py
-#mn --custom topo.py --topo=mytopo
-#sudo mn --controller=remote,ip=127.0.0.1 --switch=ovsk,protcols=OpenFlow13 --topo=single,5 --mac
